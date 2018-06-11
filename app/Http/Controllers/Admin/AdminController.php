01abd04930dd13fa0321908057a647502ab00064
cheng120 <wolfsix111@163.com>
2018-06-11 14:04:51 +0800

    admin 10%

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,13 +10,53 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\BBaseController;
+use App\Model\Admin_user;
 
 class AdminController extends BBaseController
 {
+
     /*
      * 后台登陆页面
      */
     public function adminLogin() {
+        //ip限制非IP列表不能进入
         return view("admin/login/adminLogin");
     }
+    /*
+     * 后台登陆
+     */
+    public function dologin(Request $request) {
+        $username=trim($request->input("username"));
+        $password=trim($request->input("password"));
+        $msg = array("code"=>1000,"msg"=>"登陆成功");
+
+        if(!$username || !$password){
+            $msg = array("code"=>1002,"msg"=>"登录名或密码不能为空");
+            return $msg;
+        }
+        $ad_user_model = new Admin_user();
+        $where = array(
+            "logname"=>$username,
+            "logpass"=>md5($password),
+        );
+        $userInfo = $ad_user_model->findUser($where);
+        if(!$userInfo){
+            $msg = array("code"=>1001,"msg"=>"登陆失败","debug"=>"密码错误");
+            return $msg;
+        }else{
+            $msg['data'] = $userInfo;
+            //写入登陆状态
+            //执行登陆
+            return $msg;
+        }
+    }
+    /*
+     * 登陆写入session 并更新登陆信息
+     */
+    private function writeDataForLogin($userInfo) {
+        $res = session(["userInfo"=>$userInfo]);
+        session()->save();
+        return $res;
+    }
+
 }
--- app/Model/Admin_user.php
+++ app/Model/Admin_user.php
@@ -13,4 +13,15 @@ class Admin_user extends model_base
 {
     public $timestamps = false;
     public $table = "admin_user";
+    public function __construct()
+    {
+        if(empty($this->model_table)){
+            $this->model_table = DB::table($this->table);
+        }
+    }
+    //获取管理员信息
+    public function findUser($where){
+        $userInfo = $this->model_table->where($where)->first();
+        return $userInfo;
+    }
 }
